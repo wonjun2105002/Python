d617e781601d4775440aca62c4270962e3c8f08a
wonjun2105002 <wonjunchoi1111@gmail.com>
2024-06-22 12:21:15 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,19 @@
+x = int(input())
+y = int(input())
+
+
+if x * -1 > 0 and y * -1 > 0:
+    print("3")
+    
+elif x * -1 < 0 and y * -1 < 0:
+    print("1")
+
+elif x > 0 and y < 0:
+    print("4")
+
+elif x < 0 and y > 0:
+    print ("2")
+
+
+
+

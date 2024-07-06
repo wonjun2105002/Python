e57d85065c4efbedd5afe00c27d81767b92345b3
wonjun2105002 <wonjunchoi1111@gmail.com>
2024-07-06 12:52:12 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,2 @@
+a = input()
+print(a.count("a"), a.count("b"), a.count("c"), a.count("d"), a.count("e"), a.count("f"), a.count("g"), a.count("h"), a.count("i"), a.count("j"), a.count("k"), a.count("l"), a.count("m"), a.count("n"), a.count("o"), a.count("p"), a.count("q"), a.count("r"), a.count("s"), a.count("t"), a.count("u"), a.count("v"), a.count("w"), a.count("x"), a.count("y"), a.count("z"))

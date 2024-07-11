fef4b353b9e5a6cd9c1a580537659cf8b31b014e
wonjun2105002 <wonjunchoi1111@gmail.com>
2024-07-11 16:33:02 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.py
@@ -1,11 +1,10 @@
-a,b = map(int, input().split())
+def cal (a,b):
+    if a>b:
+        print(">")
+    elif a<b:
+        print('<')
+    else:
+        print('==')
+a, b = map(int, input().split())
+cal(a,b)
 
-
-if a>b:
-    print(">")
-
-elif a<b:
-    print("<")
-
-else:
-    print("==")

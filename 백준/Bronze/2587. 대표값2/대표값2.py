9bf45981b366afefe467d96fca23e611627718b4
wonjun2105002 <wonjunchoi1111@gmail.com>
2024-07-25 15:08:27 +0900

    [Bronze II] Title: 대표값2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2587. 대표값2/대표값2.py
@@ -0,0 +1,10 @@
+a =int(input())        
+b =int(input())        
+c =int(input())        
+d =int(input())        
+e =int(input())
+
+numbers = [a,b,c,d,e]
+
+print((a+b+c+d+e)//5)
+print(sorted(numbers)[2])

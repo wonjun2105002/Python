7df2d25e73b2580b4207afc2fec9e4a98d6f894a
wonjun2105002 <wonjunchoi1111@gmail.com>
2024-07-27 12:28:33 +0900

    [Bronze II] Title: 저항, Time: 32 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+color = ["black","brown","red","orange","yellow","green","blue","violet","grey","white"] 
+
+a = color.index(input())
+b = color.index(input())
+c = 10**color.index(input())
+
+print((a*10+b)*c)

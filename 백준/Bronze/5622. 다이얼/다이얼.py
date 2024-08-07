887497f05292449ea88fad058b004f4a12233c6b
wonjun2105002 <wonjunchoi1111@gmail.com>
2024-08-07 19:22:07 +0900

    [Bronze II] Title: 다이얼, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5622. 다이얼/다이얼.py
@@ -0,0 +1,22 @@
+a = input()
+res = 0
+for i in a:
+    if i =="A" or i == "B" or i =="C":
+        res += 3
+    if i =="D" or i == "E" or i =="F":
+        res += 4
+    if i =="G" or i == "H" or i =="I":
+        res += 5
+    if i =="J" or i == "K" or i =="L":
+        res += 6
+    if i =="M" or i == "N" or i =="O":
+        res += 7
+    if i =="P" or i == "Q" or i =="R" or i =="S":
+        res += 8
+    if i =="T" or i == "U" or i =="V":
+        res += 9
+    if i =="W" or i == "X" or i =="Y" or i =="Z":
+        res += 10
+print(res)
+    
+        

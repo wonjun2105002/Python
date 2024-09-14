23a4e32e22c9bfa0f14c323882bc8b0a05fee6e6
wonjun2105002 <wonjunchoi1111@gmail.com>
2024-09-14 16:24:00 +0900

    [Bronze IV] Title: 모음의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.py
@@ -0,0 +1,6 @@
+while True:
+    a = input()
+    if a == ("#"):
+        break
+    print (a.count("a") + a.count("e") + a.count("i") + a.count("o") +a.count("u") + a.count("A") + a.count("E") + a.count("I") + a.count("O") +a.count("U"))
+

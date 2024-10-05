f663877775ac49c78243dfd242e0353e3615f004
wonjun2105002 <wonjunchoi1111@gmail.com>
2024-10-05 17:11:40 +0900

    [Silver V] Title: 분수찾기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1193. 분수찾기/분수찾기.py
@@ -0,0 +1,14 @@
+n = int(input())
+line = 1
+while n > line :
+        n -= line
+        line += 1
+if line % 2 == 0:
+        a = n
+        b = line - n + 1
+
+elif line % 2 == 1:
+        a = line - n + 1
+        b = n
+
+print (str(a)+"/"+str(b))
